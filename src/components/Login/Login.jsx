7a57a6ce74ad87fcca1ac42488ad8d16ca68e4ff
Matt Volonnino <mvolonnino12@gmail.com>
2020-11-29 12:58:24 -0500

    added userObj for sing in and sign up

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -12,9 +12,7 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Login() {
   const [elementsObj, setElementsObj] = useState({});
-  console.log({ elementsObj });
-  const sign_in_btn = document.querySelector("#sign-in-btn");
-  const sign_up_btn = document.querySelector("#sign-up-btn");
+  const [userObj, setUserObj] = useState({});
 
   const addSignUpMode = () => {
     elementsObj.container.classList.add("sign-up-mode");
@@ -24,11 +22,22 @@ function Login() {
     elementsObj.container.classList.remove("sign-up-mode");
   };
 
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    console.log({ userObj });
+  };
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    console.log({ userObj });
+  };
+
   useEffect(() => {
     setElementsObj({
       container: document.querySelector(".login"),
     });
   }, []);
+
   return (
     <div className="login">
       <div className="forms-container">
@@ -40,15 +49,32 @@ function Login() {
               <div className="icon">
                 <FontAwesomeIcon icon={faUser} />
               </div>
-              <input type="text" placeholder="Username" />
+              <input
+                type="text"
+                placeholder="Username"
+                onChange={(e) =>
+                  setUserObj({ ...userObj, username: e.target.value })
+                }
+              />
             </div>
             <div className="input-field">
               <div className="icon">
                 <FontAwesomeIcon icon={faLock} />
               </div>
-              <input type="password" placeholder="Password" />
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) =>
+                  setUserObj({ ...userObj, password: e.target.value })
+                }
+              />
             </div>
-            <button type="submit" value="Login" className="button">
+            <button
+              type="submit"
+              value="Login"
+              className="button"
+              onClick={handleSignIn}
+            >
               Login
             </button>
             {/* social sign in */}
@@ -69,21 +95,44 @@ function Login() {
               <div className="icon">
                 <FontAwesomeIcon icon={faUser} />
               </div>
-              <input type="text" placeholder="Username" />
+              <input
+                type="text"
+                placeholder="Username"
+                onChange={(e) =>
+                  setUserObj({ ...userObj, username: e.target.value })
+                }
+              />
             </div>
             <div className="input-field">
               <div className="icon">
                 <FontAwesomeIcon icon={faEnvelope} />
               </div>
-              <input type="text" placeholder="Email" />
+              <input
+                type="text"
+                placeholder="Email"
+                onChange={(e) =>
+                  setUserObj({ ...userObj, email: e.target.value })
+                }
+              />
             </div>
             <div className="input-field">
               <div className="icon">
                 <FontAwesomeIcon icon={faLock} />
               </div>
-              <input type="password" placeholder="Password" />
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) =>
+                  setUserObj({ ...userObj, password: e.target.value })
+                }
+              />
             </div>
-            <button type="submit" value="Sign up" className="button">
+            <button
+              type="submit"
+              value="Sign up"
+              className="button"
+              onClick={handleSignUp}
+            >
               Sign Up
             </button>
             {/* social sign up */}
